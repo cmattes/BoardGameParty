79630880d395e201def633c66d7f181adefb274a
cmattes <coreymattes@gmail.com>
2024-11-22 21:00:13 -0600

    Updated add button
    Updated add button to navigate to a new page for adding a new game. Updated top portion of grid to display the empty info better when a game is not selected yet.

--- BoardGameParty/Views/BoardGamesPage.xaml.cs
+++ BoardGameParty/Views/BoardGamesPage.xaml.cs
@@ -5,5 +5,8 @@ public partial class BoardGamesPage : ContentPage
     public BoardGamesPage()
     {
         InitializeComponent();
+        
+        BtnAdd.Clicked += async (s, e) => await Navigation.PushAsync(new SaveBoardGamePage(false));
+        //BtnUpdate.Clicked += async (s, e) => await Navigation.PushAsync(new SaveBoardGamePage(true));
     }
 }
